db8bee26175fba250e212f2e0031a15084440123
damianfral <huevofritopamojarpan@gmail.com>
2023-04-30 22:52:30 +0000

    Revert "Try dekking."
    This reverts commit b38d1615cbf6fc2fa0a35c216e2e44ff5d718a41.

--- haskell/flake.nix
+++ haskell/flake.nix
@@ -5,8 +5,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
     nix-filter.url = "github:numtide/nix-filter";
-    dekking.url = "github:NorfairKing/dekking";
-    safe-coloured-text.url = "github:NorfairKing/safe-coloured-text";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
     pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -16,8 +14,6 @@
     , nixpkgs
     , flake-utils
     , nix-filter
-    , dekking
-    , safe-coloured-text
     , pre-commit-hooks
     , ...
     }:
@@ -25,10 +21,7 @@
     let
       pkgsFor = system: import nixpkgs {
         inherit system;
-        overlays = [
-          self.overlays.default
-          safe-coloured-text.overlays.${system}
-        ];
+        overlays = [ self.overlays.default ];
       };
       filteredSrc =
         nix-filter.lib {
@@ -47,7 +40,6 @@
           overrides = final.lib.composeExtensions
             (old.overrides or (_: _: { }))
             (self: super: {
-              autodocodec-yaml = unmarkBroken (dontCheck super.autodocodec-yaml);
               sydtest = unmarkBroken (dontCheck super.sydtest);
               hello = self.generateOptparseApplicativeCompletions
                 [ "hello" ]
@@ -76,10 +68,6 @@
     in
     rec {
       packages.hello = pkgs.haskellPackages.hello;
-      packages.hello-coverage-report = dekking.packages.${system}.default.makeCoverageReport {
-        name = "hello-coverage-report";
-        packages = [ "hello" ];
-      };
       packages.default = packages.hello;
 
       apps.hello = flake-utils.lib.mkApp {
