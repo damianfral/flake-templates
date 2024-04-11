3fadde1da3424faad127d452f8d04c064b8a8c94
damianfral <huevofritopamojarpan@gmail.com>
2024-04-11 15:13:12 +0000

    Use alejandra instead of nixpkgs-fmt.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   description = "A collection of flake templates";
 
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
@@ -9,41 +8,41 @@
     pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-
-  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks }:
-
-    let pkgsFor = system: import nixpkgs { inherit system; }; in
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = pkgsFor system;
-          precommitCheck = pre-commit-hooks.lib.${system}.run {
-            src = ./.;
-            hooks = {
-              actionlint.enable = true;
-              markdownlint.enable = true;
-              nil.enable = true;
-              nixpkgs-fmt.enable = true;
-              statix.enable = true;
-            };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    pre-commit-hooks,
+  }: let
+    pkgsFor = system: import nixpkgs {inherit system;};
+  in
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = pkgsFor system;
+        precommitCheck = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            actionlint.enable = true;
+            markdownlint.enable = true;
+            nil.enable = true;
+            alejandra.enable = true;
+            statix.enable = true;
           };
-
-        in
-        {
-          devShells.default = pkgs.mkShell
-            {
-              inherit (precommitCheck) shellHook;
-              buildInputs = with pkgs; [
-                actionlint
-                nil
-                nixpkgs-fmt
-                statix
-              ];
-            };
-          checks = { pre-commit-check = precommitCheck; };
-        }
-      ) // {
-
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          inherit (precommitCheck) shellHook;
+          buildInputs = with pkgs; [
+            actionlint
+            alejandra
+            nil
+            statix
+          ];
+        };
+        checks = {pre-commit-check = precommitCheck;};
+      }
+    )
+    // {
       templates.haskell = {
         path = ./haskell;
         description = "Template for a haskell CLI application";
